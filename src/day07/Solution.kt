ce0bfcce38f267af59e6129b122c8196f93b0644
valeriia.zakhlieniuk <valeriia.zakhlieniuk@up42.com>
2023-12-09 01:20:53 +0100

    Day 7

--- src/day07/Solution.kt
+++ src/day07/Solution.kt
@@ -3,6 +3,85 @@ package day07
 import println
 import readInput
 
+fun main() {
+    val day = "07"
+
+    fun part1(input: List<String>): Int {
+        val result = input.map { line ->
+            val (hand, bid) = line.split(" ")
+            val case = hand.evaluateHand()
+            Input(hand, bid.toInt(), case)
+        }.sortedWith(compareBy<Input> { it.case.value }.thenComparator { a, b ->
+            compareHands(a.hand, b.hand) {
+                when (this) {
+                    '2' -> 2
+                    '3' -> 3
+                    '4' -> 4
+                    '5' -> 5
+                    '6' -> 6
+                    '7' -> 7
+                    '8' -> 8
+                    '9' -> 9
+                    'T' -> 10
+                    'J' -> 11
+                    'Q' -> 12
+                    'K' -> 13
+                    'A' -> 14
+                    else -> throw IllegalArgumentException("")
+                }
+            }
+        })
+
+        return result.mapIndexed { index, foo ->
+            foo.bid * (index + 1)
+        }.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        val result = input.map { line ->
+            val (hand, bid) = line.split(" ")
+            val case = hand.evaluateHand2()
+            Input(hand, bid.toInt(), case)
+        }.sortedWith(compareBy<Input> { it.case.value }.thenComparator { a, b ->
+            compareHands(a.hand, b.hand) {
+                when (this) {
+                    'J' -> 1
+                    '2' -> 2
+                    '3' -> 3
+                    '4' -> 4
+                    '5' -> 5
+                    '6' -> 6
+                    '7' -> 7
+                    '8' -> 8
+                    '9' -> 9
+                    'T' -> 10
+                    'Q' -> 12
+                    'K' -> 13
+                    'A' -> 14
+                    else -> throw IllegalArgumentException("")
+                }
+            }
+        })
+
+        return result.mapIndexed { index, foo ->
+            foo.bid * (index + 1)
+        }.sum()
+    }
+
+    // Part 1
+    val testInput1 = readInput(day, "test_input_1")
+    check(part1(testInput1) == 6440)
+
+    // Part 2
+    val testInput2 = readInput(day, "test_input_2")
+    check(part2(testInput2) == 5905)
+
+    println("Solution:")
+    val input = readInput(day, "input")
+    part1(input).println()
+    part2(input).println()
+}
+
 enum class Case(val value: Int) {
     FIVE_OF_THE_KIND(6),
     FOUR_OF_THE_KIND(5),
@@ -28,59 +107,23 @@ fun String.evaluateHand(): Case {
 }
 
 fun String.evaluateHand2(): Case {
-    val jCount = this.toCharArray().count{ it == 'J' }
+    val jCount = this.toCharArray().count { it == 'J' }
     val chars = this.toCharArray().filter { it != 'J' }.groupBy { it }
 
     return when {
-        chars.keys.size <= 1  -> Case.FIVE_OF_THE_KIND
+        chars.keys.size <= 1 -> Case.FIVE_OF_THE_KIND
         chars.values.any { it.size + jCount == 4 } -> Case.FOUR_OF_THE_KIND
         chars.keys.size == 2 -> Case.FULL_HOUSE
         chars.values.any { it.size + jCount == 3 } -> Case.THREE_OF_THE_KIND
         chars.values.count { it.size == 2 } == 2 -> Case.TWO_PAIRS
-        chars.values.any { it.size + jCount == 2 }  -> Case.ONE_PAIR
+        chars.values.any { it.size + jCount == 2 } -> Case.ONE_PAIR
         else -> Case.HIGH_CARD
     }
 }
 
-data class Foo(val hand: String, val bid: Int, val case: Case)
+data class Input(val hand: String, val bid: Int, val case: Case)
 
-fun Char.cardValue() =
-    when (this) {
-        '2' -> 2
-        '3' -> 3
-        '4' -> 4
-        '5' -> 5
-        '6' -> 6
-        '7' -> 7
-        '8' -> 8
-        '9' -> 9
-        'T' -> 10
-        'J' -> 11
-        'Q' -> 12
-        'K' -> 13
-        'A' -> 14
-        else -> throw IllegalArgumentException("")
-    }
-
-fun Char.cardValue2() =
-    when (this) {
-        'J' -> 1
-        '2' -> 2
-        '3' -> 3
-        '4' -> 4
-        '5' -> 5
-        '6' -> 6
-        '7' -> 7
-        '8' -> 8
-        '9' -> 9
-        'T' -> 10
-        'Q' -> 12
-        'K' -> 13
-        'A' -> 14
-        else -> throw IllegalArgumentException("")
-    }
-
-fun compareHands(first: String, second: String): Int {
+fun compareHands(first: String, second: String, cardValue: Char.() -> Int): Int {
     (0 until 5).forEach { i ->
 
         if (first[i].cardValue() < second[i].cardValue()) {
@@ -90,63 +133,4 @@ fun compareHands(first: String, second: String): Int {
         }
     }
     return 0
-}
-
-fun compareHands2(first: String, second: String): Int {
-    (0 until 5).forEach { i ->
-
-        if (first[i].cardValue2() < second[i].cardValue2()) {
-            return -1
-        } else if (first[i].cardValue2() > second[i].cardValue2()) {
-            return 1
-        }
-    }
-    return 0
-}
-
-fun main() {
-    val day = "07"
-
-    fun part1(input: List<String>): Int {
-        val result = input.map { line ->
-            val (hand, bid) = line.split(" ")
-            val case = hand.evaluateHand()
-            Foo(hand, bid.toInt(), case)
-        }.sortedWith(compareBy<Foo> { it.case.value }.thenComparator { a, b ->
-            compareHands(a.hand, b.hand)
-        })
-
-
-        return result.mapIndexed { index, foo ->
-            foo.bid * (index + 1)
-        }.sum()
-    }
-
-    fun part2(input: List<String>): Int {
-        val result = input.map { line ->
-            val (hand, bid) = line.split(" ")
-            val case = hand.evaluateHand2()
-            Foo(hand, bid.toInt(), case)
-        }.sortedWith(compareBy<Foo> { it.case.value }.thenComparator { a, b ->
-            compareHands2(a.hand, b.hand)
-        })
-
-
-        return result.mapIndexed { index, foo ->
-            foo.bid * (index + 1)
-        }.sum()
-    }
-
-    // Part 1
-    val testInput1 = readInput(day, "test_input_1")
-    check(part1(testInput1) == 6440)
-
-    // Part 2
-    val testInput2 = readInput(day, "test_input_2")
-    check(part2(testInput2) == 5905)
-
-    println("Solution:")
-    val input = readInput(day, "input")
-    part1(input).println()
-    part2(input).println()
 }
